2760bf1bfda21b57a16ed8caebe5d702149e225d
Andrew Wittrock <andy@wittrock.us>
2015-11-21 21:18:26 -0600

    Update styling

--- src/ATPAdvertising/View/Helper/Adsense.php
+++ src/ATPAdvertising/View/Helper/Adsense.php
@@ -10,11 +10,12 @@ class Adsense extends \ATP\View\Helper
 		$ad->loadByIdentifier($id);
 		
 		return "
-			<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
 			<ins class=\"adsbygoogle\"
-				 style=\"display:inline-block;width:{$ad->width}px;height:{$ad->height}px\"
+				 style=\"display:inline-block;\"
+				 data-format=\"auto\"
 				 data-ad-client=\"{$ad->publisher}\"
 				 data-ad-slot=\"{$ad->adId}\"></ins>
+			<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
 			<script>
 			(adsbygoogle = window.adsbygoogle || []).push({});
 			</script>
