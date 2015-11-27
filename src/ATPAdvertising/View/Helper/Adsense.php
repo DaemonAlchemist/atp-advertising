9e0fd53b4cf4ad6115756390efa7cc6837cb8ac1
Andrew Wittrock <andy@wittrock.us>
2015-11-26 23:42:29 -0600

    Remove explicit sizes from adsense ads

--- src/ATPAdvertising/View/Helper/Adsense.php
+++ src/ATPAdvertising/View/Helper/Adsense.php
@@ -12,7 +12,7 @@ class Adsense extends \ATP\View\Helper
 		return "
 			<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
 			<ins class=\"adsbygoogle\"
-				 style=\"display:inline-block;width:{$ad->width}px;height:{$ad->height}px\"
+				 style=\"display:inline-block;\"
 				 data-ad-client=\"{$ad->publisher}\"
 				 data-ad-slot=\"{$ad->adId}\"></ins>
 			<script>
